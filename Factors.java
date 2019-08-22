b07b4d4d4df45851808c2bbc5168993c5c9fdc74
Conlan Cason <conlan.cason9@gmail.com>
2019-08-21 20:38:16 -0600

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,24 @@
+package conlan.math;
+import java.util.ArrayList;
+
+public class Factors 
+{
+	
+	public static ArrayList<Couple> getFactors(int num)
+	{
+		ArrayList<Couple> factors = new ArrayList<>();
+		
+		for (int i = num-1; i >= Math.sqrt(num); i--)
+		{
+			for (int k = 2; k <= Math.sqrt(num); k++)
+			{
+				if (i * k == num)
+				{
+					factors.add(new Couple(i, k));	
+				}
+			}
+		}
+		
+		return factors;
+	}
+}
