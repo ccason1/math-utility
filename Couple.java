3754679f19b31c8352b0a646fd47a3088e9287c1
Conlan Cason <conlan.cason9@gmail.com>
2019-08-21 20:39:06 -0600

    Add files via upload

---
+++ Couple.java
@@ -0,0 +1,52 @@
+/**
+ *@author Conlan Cason
+ *@version 1.0
+ *
+ *container two <code>int</code>s
+ *
+ *@param a an <code>int</code> a
+ *@param b an <code>int</code> b
+*/
+
+
+package conlan.math;
+
+public class Couple {
+	public int a;
+	public int b;
+	
+	public Couple(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public String toString()
+	{
+		return "a: " + a + " b: " + b;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if(this == obj)
+	        return true;
+
+	    if(obj == null || obj.getClass() != this.getClass())
+	        return false;
+
+	    Couple couple = (Couple) obj;
+	    
+	    //TODO rename these four booleans
+	    
+	    boolean firstConditionOne = this.a == couple.a;
+	    boolean firstConditionTwo = this.b == couple.b;
+	    
+	    boolean secondConditionOne = this.a == couple.b;
+	    boolean secondConditionTwo = this.b == couple.a;
+	    
+	    if (firstConditionOne && firstConditionTwo || secondConditionOne && secondConditionTwo)
+	        return true;
+
+	    return false;
+	}
+}
